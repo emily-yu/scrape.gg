b32f3028d2b71db8696c635cff78079d5a774846
Emily Yu <eyudeveloper@gmail.com>
2020-02-18 01:04:38 -0800

    add remote click

--- init.py
+++ init.py
@@ -16,3 +16,19 @@ driver = webdriver.Chrome(driverpath, options=chrome_options)
 driver.get('https://na.op.gg/summoner/userName=' + summonerName)
 
 print(driver.page_source)
+
+# block [1] get last updated time for checking updated click for button
+a = driver.find_elements_by_class_name("LastUpdate")
+print(a[0].text)
+
+updatebutton = driver.find_elements_by_id("SummonerRefreshButton")[0]
+ActionChains(driver).click(updatebutton).perform()
+# WebDriverWait(driver, 15).until(a != driver.find_elements_by_class_name("LastUpdate")[0].text)
+WebDriverWait(driver, 10).until(
+    lambda wd: a == driver.find_elements_by_class_name("LastUpdate")[0].text
+)
+
+# verify that [1] has changed
+b = driver.find_elements_by_class_name("LastUpdate")
+print(b[0].text)
+print('driver update successful: ', a == b)
